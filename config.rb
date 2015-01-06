dd8e5605b8174b65d5b9d2dae1de97044b4826d4
dusanmilko <dusanmilko@hotmail.com>
2015-01-06 16:39:39 -0500

    inital project

---
+++ config.rb
@@ -0,0 +1,35 @@
+# Require any additional compass plugins here.
+require "sass-globbing"
+require 'breakpoint'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "scss"
+images_dir = "assets/imgs"
+javascripts_dir = "assets/js"
+sprite_load_path = "assets/imgs"
+#generated_images_dir = "/"
+http_generated_images_path = "../imgs/"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+# Trigger blessc after compilation
+# on_stylesheet_saved do |filename|
+#     exec('blessc ' + css_dir + '/' + File.basename(filename) + ' -f')
+# end
---
+++ index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie10 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie10 lt-ie9"> <![endif]-->
+<!--[if IE 9]>         <html class="no-js lt-ie10"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PB Companies</title>
+	<meta name="description" content="TODO">
+	<meta name="keywords" content="TODO">
+
+	<!-- og tags for FB share -->
+	<meta property="og:title" content="TODO"/>
+	<meta property="og:image" content="TODO"/>
+	<meta property="og:site_name" content="TODO"/>
+	<meta property="og:description" content="TODO"/>
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+	<link rel="stylesheet" href="/assets/css/main.css">
+
+	<script src="/js/libs/modernizr.min.js"></script>
+
+	<!--[if lt IE 9]>
+	  <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
+	  <script src="//s3.amazonaws.com/nwapi/nwmatcher/nwmatcher-1.2.5-min.js"></script>
+	  <script src="//html5base.googlecode.com/svn-history/r38/trunk/js/selectivizr-1.0.3b.js"></script>
+	  <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<!--[if lt IE 8]>
+		<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+
+	<!--content-->
+
+	<div class="row">
+		<div class="header">
+			<ul>
+				<li>
+					Projects
+				</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="rel">
+			<img class="hunw fleft" src="http://placehold.it/1050x450"/>
+			<div class="abs">
+				<div class="table">
+					<div class="table-cell">
+						Building the Central Coast
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="small-12 medium-4 fleft">
+			<h4>
+				hi
+			</h4>
+			<p class="center">
+				hmm
+			</p>
+		</div>
+		<div class="small-12 medium-4 fleft">
+			hi
+		</div>
+		<div class="small-12 medium-4 fleft">
+			hi
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="small-12 medium-6 fleft">
+			<img class="hunw" src="http://placehold.it/350x150"/>
+		</div>
+		<div class="small-12 medium-6 fleft">
+			<img class="hunw" src="http://placehold.it/350x150"/>
+		</div>
+		<div class="small-12 medium-4 fleft">
+			<img class="hunw" src="http://placehold.it/350x150"/>
+		</div>
+		<div class="small-12 medium-4 fleft">
+			<img class="hunw" src="http://placehold.it/350x150"/>
+		</div>
+		<div class="small-12 medium-4 fleft">
+			<img class="hunw" src="http://placehold.it/350x150"/>
+		</div>
+	</div>
+
+	<div class="row">
+		<h5 class="caps">
+			Who We Are
+		</h5>
+		<p class="hunw">
+			text
+		</p>
+		<p class="hunw">
+			text
+		</p>
+	</div>
+
+	<div class="row">
+		<div class="small-12 columns">
+			<ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4">
+				<li>
+					<div class="rel hunw">
+						<img class="hunw" src="http://placehold.it/250x250"/>
+						<div class="abs">
+							hmmmm
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="small-12 columns">
+			<ul class="small-block-grid-2 medium-block-grid-2 large-block-grid-4">
+				<li>
+					<img class="hunw" src="http://placehold.it/250x250"/>
+				</li>
+				<li>
+					<img class="hunw" src="http://placehold.it/250x250"/>
+				</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="rel">
+			<img class="hunw fleft" src="http://placehold.it/1050x350"/>
+			<div class="abs">
+				<div class="table">
+					<div class="table-cell">
+						A Build Boom in Slo
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!--end content-->
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="/js/app.js"></script>
+
+</body>
+</html>
